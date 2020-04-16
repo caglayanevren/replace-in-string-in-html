df0702c57bfbcec52929147cd2fa23681fb53881
Evren Caglayan <caglayanevren@gmail.com>
2020-04-16 18:29:58 +0300

    regex update

--- postbuild.js
+++ postbuild.js
@@ -2,23 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const replace = require('replace-in-file');
 
-let baseDir = 'dist'
+let baseDir = 'dist/en'
 
 fs.readdir(`./${baseDir}`, (err, files) => {
     let html = []
     let fromtoRegExp = [
-        {from:'(src=")(.*)(\.js")', to:'$1../assets/$2$3'},
-        {from:'(href=")(.*)(\.css")',to:'$1../assets/$2$3'},
-        {from:'(src=")(.*)(\.png")', to:'$1../assets/images/$2$3'},
-        {from:'(src=")(.*)(\.svg")', to:'$1../assets/images/$2$3'},
-        {from:'(src=")(.*)(\.jpg")', to:'$1../assets/images/$2$3'},
-        {from:'(href=")(.*)(\.ico")', to:'$1../assets/images/$2$3'},
-        {from:'(srcset=")(.*)(\.webp")', to:'$1../assets/images/$2$3'},
-        {from:'(srcset=")(.*)(\.jpg")', to:'$1../assets/images/$2$3'},
-        {from:'(srcset=")(.*)(\.png")', to:'$1../assets/images/$2$3'},
-        {from:'(href=")en/(.*)(.html")', to:'$1$2$3'},
-        {from:'(<a href=“)(.*)(\.html" class="nav-link" id="trtoen">TR</a>)', to:'$1../$2$3',},
-        {from:'(href=“)(pdf/)(.*)(.pdf”)', to:'$1../$2$3$4'},
+        {from:'(src=")([^ ]+\.js")', to:'$1../assets/$2'},
+        {from:'(href=")([^ ]+\.css")',to:'$1../assets/$2'},
+        {from:'(src=")([^ ]+\.png")', to:'$1../assets/images/$2'},
+        {from:'(src=")([^ ]+\.svg")', to:'$1../assets/images/$2'},
+        {from:'(src=")([^ ]+\.jpg")', to:'$1../assets/images/$2'},
+        {from:'(href=")([^ ]+\.ico")', to:'$1../assets/images/$2'},
+        {from:'(srcset=")([^ ]+\.webp")', to:'$1../assets/images/$2'},
+        {from:'(srcset=")([^ ]+\.jpg")', to:'$1../assets/images/$2'},
+        {from:'(srcset=")([^ ]+\.png")', to:'$1../assets/images/$2'},
+        {from:'(href=")en/([^ ]+\.html")', to:'$1$2'},
+        {from:'(<a href=")([^ ]+\.html" class="nav-link" id="trtoen">TR</a>)', to:'$1../$2',},
+        {from:'(href=")(pdf\/[^ ]+\.pdf")', to:'$1../$2'},
     ]
 
     files.forEach(file => {
@@ -28,7 +28,7 @@ fs.readdir(`./${baseDir}`, (err, files) => {
         }
     });
 
-    console.log('html', html)
+    console.log('html:', html)
 
     html.forEach(file => {
         fromtoRegExp.forEach(reg => {
@@ -39,6 +39,7 @@ fs.readdir(`./${baseDir}`, (err, files) => {
                 }
                 try {
                     let changedFiles = replace.sync(options);
+                    console.log('FROM: ', reg.from , '--> TO: ', reg.to);
                     console.log('Modified files:', changedFiles.join(', '));
                 } catch (error) {
                     console.error('Error occurred:', error);
